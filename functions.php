21d2ac64246076e821e83c3a85184865e4c38cdd
bryher <bryher_msm@hotmail.com>
2020-03-09 15:27:20 -0500

    m

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 function agregar_css_js()
 {
 wp_enqueue_style( 'style', get_stylesheet_uri() );
-wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '1.1', 'all');
-p_enqueue_script( 'script', get_template_directory_uri() . '/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
+wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+wp_enqueue_script( 'script', get_template_directory_uri() . '/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
 
 }
 add_action('wp_enqueue_scripts', 'agregar_css_js');
